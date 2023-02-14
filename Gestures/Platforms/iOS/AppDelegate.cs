8cc1e61bbcb5f0696f367ede2e4f917c93cfb332
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-02-14 09:44:08 +0100

    Initial commit

---
+++ Gestures/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Gestures;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
